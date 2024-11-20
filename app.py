3547a562785074dcbebbb328726e9532639c7852
RemyHrz <55578585+RemyHrz@users.noreply.github.com>
2024-11-20 09:38:52 +0100

    first commit
    commit all files

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, session, flash
+import tomli
+from datetime import timedelta
+from models import db
+from tools import logged_in, flash_success, get_visit_count, check_account, check_login, create_account
+
+app = Flask(__name__)
+
+with open("config.toml", "rb") as file:
+    config_secrets = tomli.load(file)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = config_secrets['SECRET_KEY']
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
+app.config['SESSION_COOKIE_SECURE']=True
+app.config['SESSION_COOKIE_HTTPONLY']=True
+app.config['SESSION_COOKIE_SAMESITE']='Lax'
+
+db.init_app(app)
+
+app.app_context().push()
+with app.app_context():
+    db.create_all()
+
+@app.route("/", methods=["GET","POST"])
+def mainpage():
+    if request.method == "POST":
+        check_account()
+        create_account()
+        flash(flash_success("Votre compte a été créé"))
+        return redirect("/")
+    else:
+        try:
+            username=session["username"]
+            email=session["email"]
+            visit_count=get_visit_count(username)
+            return render_template("index.html", username=username, email=email, visit_count=visit_count)
+        except:
+            return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login_page():
+    if request.method == "POST":
+        check_login()
+        return redirect("/")
+    else:
+        return redirect("/")
+
+@app.route("/logout")
+@logged_in
+def logout():
+    session.clear()
+    return redirect("/")
+
+if __name__=="__main__":
+	app.run(debug=True)
---
+++ tools.py
@@ -0,0 +1,77 @@
+from functools import wraps
+from flask import request, session, redirect, flash
+from models import db, User
+
+def flash_error(txt):
+    return flash_message(txt, "alert-danger")
+
+def flash_success(txt):
+    return flash_message(txt, "alert-success")
+
+def flash_message(txt, alert_type):
+    return """<div class="d-inline-block alert """ + alert_type + """ alert-dismissible fade show" role="alert">
+  """+txt+"""<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>"""
+
+def logged_in(f):
+    @wraps(f)
+    def decorated_func(*args, **kwargs):
+        if 'username' in session:
+            username = session['username']
+            user = User.query.filter(User.username==username).first()
+            if user:
+                return f(*args, **kwargs)
+            else:
+                return redirect("/logout")
+        else:
+            return redirect("/")
+    return decorated_func
+
+def get_visit_count(username):
+    user = User.query.filter(User.username==username).first()
+    visit = user.visits
+    return visit
+
+def visit_counter(username):
+    user = User.query.filter(User.username==username).first()
+    user.visits+=1
+    db.session.commit()
+
+def create_account():
+    username, password, email = process_form()
+    user = User(username=username, email=email)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+def process_form():
+    username = request.form["username"].strip()
+    try:
+        email = request.form["email"].strip()
+    except:
+        password = request.form["password"]
+        return username, password
+    try:
+        password = request.form["password"]
+    except:
+        return username, email
+    return username, password, email
+
+def check_account():
+    username, email, none = process_form()
+    user = User.query.filter(User.username==username).first()
+    mail = User.query.filter(User.email==email).first()
+    if user or mail:
+        flash(flash_error("Ce nom d'utilisateur ou cette adresse mail est déja utilisée!"))
+        return redirect("/")
+
+def check_login():
+    username, password = process_form()
+    user = User.query.filter(User.username==username).first()
+    if user and user.check_password(password):
+        session["username"]=username
+        session["email"]=user.email
+        visit_counter(username)
+        session.permanent=True
+        flash(flash_success("connexion réussie"))
+    else:
+        flash(flash_error("Le mot de passe est incorrect"))
